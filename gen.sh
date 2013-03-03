2258eb5e150124febea07f510f9bfadaa589626c
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-03-03 17:23:02 -0500

    States.

--- gen.sh
+++ gen.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ROOT=~/Data/Maps
 COUNTRIES=ne_50m_admin_0_countries
+rm "countries.json"
 ogr2ogr -f GeoJSON "countries.json" -where "su_a3 != 'ATA'" "$ROOT/$COUNTRIES/ne_50m_admin_0_countries.shp"
+topojson --id-property su_a3 -p name -o countries.topo.json countries.json
 
 STATES=ne_50m_admin_1_states_provinces_lakes
-ogr2ogr -f GeoJSON "states.json" -where "sr_adm0_a3 == 'USA'" "$ROOT/$STATES/$STATES.shp"
-
-topojson --id-property su_a3 -p name -o countries.topo.json countries.json
+rm states.json
+ogr2ogr -f GeoJSON "states.json" -where "sr_adm0_a3 = 'USA'" "$ROOT/$STATES/${STATES}_shp.shp"
+topojson --id-property postal -p name -o states.topo.json states.json
