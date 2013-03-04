e1e93a48cff559e15a161644a1c80c7d9db75409
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-03-03 20:07:21 -0500

    Try a different kind of projection.

--- gen.sh
+++ gen.sh
@@ -2,7 +2,7 @@
 ROOT=~/Data/Maps
 COUNTRIES=ne_50m_admin_0_countries
 rm "countries.json"
-ogr2ogr -f GeoJSON "countries.json" -where "su_a3 NOT IN ('ATA', 'GRL')" "$ROOT/$COUNTRIES/ne_50m_admin_0_countries.shp"
+ogr2ogr -f GeoJSON "countries.json" -where "su_a3 NOT IN ('ATA', 'GRL', 'USA', 'CAN')" "$ROOT/$COUNTRIES/ne_50m_admin_0_countries.shp"
 topojson --id-property su_a3 -p name -o countries.topo.json countries.json
 
 STATES=ne_50m_admin_1_states_provinces_lakes
