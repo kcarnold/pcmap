28879d3c7f0745c166be47458d8de4bf14c03c68
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-03-04 22:48:05 -0500

    Simplify the map geometry.

--- gen.sh
+++ gen.sh
@@ -3,9 +3,9 @@ ROOT=~/Data/Maps
 COUNTRIES=ne_50m_admin_0_countries
 rm "countries.json"
 ogr2ogr -f GeoJSON "countries.json" -where "su_a3 NOT IN ('ATA', 'GRL', 'USA', 'CAN')" "$ROOT/$COUNTRIES/ne_50m_admin_0_countries.shp"
-topojson --id-property su_a3 -p name -o countries.topo.json countries.json
+topojson --id-property su_a3 -p name --simplify-proportion .25 -o countries.topo.json countries.json
 
 STATES=ne_50m_admin_1_states_provinces_lakes
 rm states.json
 ogr2ogr -f GeoJSON "states.json" -where "sr_adm0_a3 = 'USA'" "$ROOT/$STATES/${STATES}_shp.shp"
-topojson --id-property postal -p name -o states.topo.json states.json
+topojson --id-property postal -p name --simplify-proportion .5 -o states.topo.json states.json
